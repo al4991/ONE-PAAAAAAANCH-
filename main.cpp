e1bc85f01ee0cf80d504b7791545fbf010764462
Andrew <al4991@nyu.edu>
2019-05-10 15:21:38 -0400

    Fixed dangling pointer issue

--- main.cpp
+++ main.cpp
@@ -45,23 +45,23 @@ float lerp(float v0, float v1, float t) {
 GLuint LoadTexture(const char *filePath) {
     int w, h, comp;
     unsigned char* image = stbi_load(filePath, &w, &h, &comp, STBI_rgb_alpha);
-    
+
     if (image == NULL) {
         std::cout << "Unable to load image. Make sure the path is correct\n";
-        
+
     }
-    
+
     GLuint retTexture;
     glGenTextures(1, &retTexture);
     glBindTexture(GL_TEXTURE_2D, retTexture);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-    
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    
+
     stbi_image_free(image);
     return retTexture;
 }
@@ -90,7 +90,7 @@ public:
         }
         delete mapData;
     }
-    
+
     void Load(const std::string fileName) {
         std::ifstream infile(fileName);
         if (infile.fail()) {
@@ -111,14 +111,14 @@ public:
             }
         }
     }
-    
+
     int mapWidth;
     int mapHeight;
     unsigned int **mapData;
     std::vector<FlareMapEntity> entities;
-    
+
 private:
-    
+
     bool ReadHeader(std::ifstream &stream) {
         std::string line;
         mapWidth = -1;
@@ -147,7 +147,7 @@ private:
             return true;
         }
     }
-    
+
     bool ReadLayerData(std::ifstream &stream) {
         std::string line;
         while (getline(stream, line)) {
@@ -176,7 +176,7 @@ private:
         }
         return true;
     }
-    
+
     bool ReadEntityData(std::ifstream &stream) {
         std::string line;
         std::string type;
@@ -194,26 +194,26 @@ private:
                 std::string xPosition, yPosition;
                 getline(lineStream, xPosition, ',');
                 getline(lineStream, yPosition, ',');
-                
+
                 FlareMapEntity newEntity;
                 newEntity.type = type;
                 newEntity.x = std::atoi(xPosition.c_str()) * TILE_SIZE;
                 newEntity.y = std::atoi(yPosition.c_str()) * -TILE_SIZE;
                 entities.push_back(newEntity);
-                
+
                 // In the slides, x and y are multiplied by tilesize
             }
         }
         return true;
     }
-    
+
 };
 
 struct SpriteSheet {
     unsigned int textureID;
     int spriteCountX;
     int spriteCountY;
-    
+
     SpriteSheet() {}
     SpriteSheet(unsigned int textureID_, int x, int y) {
         textureID = textureID_;
@@ -232,8 +232,8 @@ public:
         height = height_;
         isStatic = isStatic_;
     }
-    
-    
+
+
     float x = 0.0f;
     float y = 0.0f;
     float width;
@@ -245,22 +245,23 @@ public:
     float fricX = 0.0f;
     float fricY = 0.0f;
     float gravityY = -1.3f;
-    
+
     SpriteSheet sheet;
+
     std::vector<int> sprites;
     int spriteIndex;
-    
+
     bool collidedTop = false;
     bool collidedBottom = false;
     bool collidedLeft = false;
     bool collidedRight = false;
     bool isStatic = true;
-    
+
     void Render(ShaderProgram& p) {
         glm::mat4 modelMatrix = glm::mat4(1.0f);
         modelMatrix = glm::translate(modelMatrix, glm::vec3(x, y, 0.0f));
         p.SetModelMatrix(modelMatrix);
-        
+
         float u = (float)(((int)sprites[spriteIndex]) % sheet.spriteCountX) / (float)sheet.spriteCountX;
         float v = (float)(((int)sprites[spriteIndex]) / sheet.spriteCountX) / (float)sheet.spriteCountY;
         float spriteWidth = 1.0 / (float)sheet.spriteCountX;
@@ -273,7 +274,6 @@ public:
             u, v + spriteHeight,
             u + spriteWidth, v + spriteHeight
         };
-        //        float aspect = width / height;
         float vertices[] = {
             -0.5f * TILE_SIZE , -0.5f * TILE_SIZE,
             0.5f * TILE_SIZE, 0.5f * TILE_SIZE,
@@ -282,22 +282,22 @@ public:
             -0.5f * TILE_SIZE, -0.5f * TILE_SIZE ,
             0.5f * TILE_SIZE , -0.5f * TILE_SIZE
         };
-        
+
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glBindTexture(GL_TEXTURE_2D, sheet.textureID);
-        
+
         glVertexAttribPointer(p.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
         glEnableVertexAttribArray(p.positionAttribute);
         glVertexAttribPointer(p.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
         glEnableVertexAttribArray(p.texCoordAttribute);
-        
+
         glDrawArrays(GL_TRIANGLES, 0, 6);
-        
+
         glDisableVertexAttribArray(p.positionAttribute);
         glDisableVertexAttribArray(p.texCoordAttribute);
     }
-    
+
     void movePlayer(float elapsed) {
         //        worldToTileCoordinates(player.x, player.y, &gridX, &gridY);
         velX = lerp(velX, 0.0f, elapsed * fricX);
@@ -308,23 +308,36 @@ public:
         x += velX * elapsed;
         y += velY * elapsed;
     }
-    
-    
+
+    void checkCollision(unsigned int **mapData) {
+        int gridX; 
+        int gridY; 
+        worldToTileCoordinates(x, y, &gridX, &gridY); 
+
+    }
+
 };
 
 struct GameState {
     SpriteSheet Texture;
     SpriteSheet FontTexture;
-    FlareMap map;
-    Entity player;
+    FlareMap map = FlareMap();
+    Entity player = Entity();
     ShaderProgram program;
-    
+
     void Render(ShaderProgram& p, float elapsed) {
+        glm::mat4 modelMatrix = glm::mat4(1.0f);
+        modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, 0.0f, 0.0f)); 
+        glm::mat4 viewMatrix = glm::mat4(1.0f); 
+        viewMatrix = glm::translate(viewMatrix, glm::vec3(-player.x, -player.y, 0.0f)); 
+        p.SetModelMatrix(modelMatrix); 
+        p.SetViewMatrix(viewMatrix); 
+
         DrawTiles();
         player.Render(p);
         player.movePlayer(elapsed);
     }
-    
+
     void DrawTiles() {
         std::vector<float> vertexData;
         std::vector<float> texCoordData;
@@ -342,7 +355,7 @@ struct GameState {
                         TILE_SIZE * x, -TILE_SIZE * y,
                         (TILE_SIZE * x) + TILE_SIZE, (-TILE_SIZE * y) - TILE_SIZE,
                         (TILE_SIZE * x) + TILE_SIZE, -TILE_SIZE * y
-                    });
+                        });
                     texCoordData.insert(texCoordData.end(), {
                         u, v,
                         u, v + (spriteHeight),
@@ -350,38 +363,37 @@ struct GameState {
                         u, v,
                         u + spriteWidth, v + (spriteHeight),
                         u + spriteWidth, v
-                    });
-                    
+                        });
+
                 }
             }
         }
-        
+
         glUseProgram(program.programID);
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        
+
         glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
         glEnableVertexAttribArray(program.positionAttribute);
-        
+
         glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
         glEnableVertexAttribArray(program.texCoordAttribute);
-        
+
         glBindTexture(GL_TEXTURE_2D, Texture.textureID);
-        glDrawArrays(GL_TRIANGLES, 0, vertexData.size()/2);
-        
+        glDrawArrays(GL_TRIANGLES, 0, vertexData.size() / 2);
+
         glDisableVertexAttribArray(program.positionAttribute);
         glDisableVertexAttribArray(program.texCoordAttribute);
     }
-    
-    
+
+
     bool checkCollision() {
-        
+
         return false;
     }
-    
+
 };
-//
-//
+
 //void DrawText(ShaderProgram &program, int fontTexture, std::string text, float size, float spacing) {
 //    float character_size = 1.0 / 16.0f;
 //
@@ -438,64 +450,56 @@ int main(int argc, char *argv[])
     displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
     SDL_GL_MakeCurrent(displayWindow, context);
-    
+
 #ifdef _WINDOWS
     glewInit();
 #endif
-    
+
     ShaderProgram program;
     program.Load(RESOURCE_FOLDER"vertex_textured.glsl", RESOURCE_FOLDER"fragment_textured.glsl");
-    
+
     glClearColor(0.039f, 0.596f, 0.674f, 1.0f);
     glm::mat4 projectionMatrix = glm::mat4(1.0f);
     glm::mat4 modelMatrix = glm::mat4(1.0f);
     glm::mat4 viewMatrix = glm::mat4(1.0f);
     projectionMatrix = glm::ortho(-1.777f, 1.777f, -1.0f, 1.0f, -1.0f, 1.0f);
+    program.SetProjectionMatrix(projectionMatrix); 
     glUseProgram(program.programID);
-    
+
     // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     SpriteSheet tiles = SpriteSheet(LoadTexture(RESOURCE_FOLDER"arne_sprites.PNG"), 16, 8);
     SpriteSheet cuties = SpriteSheet(LoadTexture(RESOURCE_FOLDER"cuties.PNG"), 6, 8);
-    
-    
-    FlareMap map;
-    map.Load(RESOURCE_FOLDER"level2.txt");
-    
+
     GameState test;
-    test.map = map;
+    test.map.Load(RESOURCE_FOLDER"level2.txt");
     test.Texture = tiles;
     test.program = program;
-    
+
     Entity player;
     player = Entity(test.map.entities[0].x, test.map.entities[0].y, TILE_SIZE, TILE_SIZE, false);
     player.sheet = cuties;
     player.spriteIndex = 0;
     player.sprites = { 16 };
-    
+
     test.player = player;
-    
-    /*for (FlareMapEntity i : test.map.entities) {
-     std::cout << i.type << std::endl;
-     }
-     std::cout << "HELLO ===============================" << std::endl; */
-    
+
     float one = 0.0f;
     float two = 0.0f;
-    
+
     SDL_Event event;
     bool done = false;
     while (!done) {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);/*
         viewMatrix = glm::mat4(1.0f);
-        viewMatrix = glm::translate(viewMatrix, glm::vec3(one, two, 0.0f));
-        //        viewMatrix = glm::translate(viewMatrix, glm::vec3(-player.x, -player.y, 0.0f));
-        
-        
+        viewMatrix = glm::translate(viewMatrix, glm::vec3(one, two, 0.0f));*/
+        //viewMatrix = glm::translate(viewMatrix, glm::vec3(-player.x, -player.y, 0.0f));
+
+
         const Uint8 *keys = SDL_GetKeyboardState(NULL);
-        float ticks = (float)SDL_GetTicks()/1000.0f;
+        float ticks = (float)SDL_GetTicks() / 1000.0f;
         float elapsed = ticks - lastFrameTicks;
         lastFrameTicks = ticks;
-        
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
                 done = true;
@@ -506,25 +510,26 @@ int main(int argc, char *argv[])
         }
         else if (keys[SDL_SCANCODE_D]) {
             one -= 0.1f;
-        }else if (keys[SDL_SCANCODE_W]) {
+        }
+        else if (keys[SDL_SCANCODE_W]) {
             two -= 0.1f;
         }
         else if (keys[SDL_SCANCODE_S]) {
             two += 0.1f;
         }
-        
-        if(keys[SDL_SCANCODE_LEFT]) {
+
+        if (keys[SDL_SCANCODE_LEFT]) {
             test.player.velX = -1.25f;
             test.player.velY = 0.0f;
             //            test.player.accY = 0.0f;
-            
+
         }
-        if(keys[SDL_SCANCODE_RIGHT]) {
+        if (keys[SDL_SCANCODE_RIGHT]) {
             test.player.velX = 1.25f;
             test.player.velY = 0.0f;
             //            test.player.accY = 0.0f;
         }
-        if(keys[SDL_SCANCODE_UP]) {
+        if (keys[SDL_SCANCODE_UP]) {
             test.player.velY = 1.25f;
             test.player.velX = 0.0f;
             if (test.player.velY > 2.0f) {
@@ -538,17 +543,15 @@ int main(int argc, char *argv[])
         //            test.player.velX = 0.0f;
         //            test.player.accX = 0.0f;
         //        }
-        
-        
-        program.SetModelMatrix(modelMatrix);
-        program.SetProjectionMatrix(projectionMatrix);
-        program.SetViewMatrix(viewMatrix);
+
+
         test.Render(program, elapsed);
-        
+
         SDL_GL_SwapWindow(displayWindow);
     }
-    
+
     SDL_Quit();
     return 0;
 }
+
 
